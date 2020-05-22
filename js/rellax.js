4f48ab597b9d49f8f0b92cd38a3b52a2d74cd643
easymoneypardi <mathias.pardi@gmail.com>
2020-05-22 12:26:44 +0200

    Version 36

--- js/rellax.js
+++ js/rellax.js
@@ -494,3 +494,11 @@
   };
   return Rellax;
 }));
+
+var rellax = new Rellax('.rellax', {
+        // center: true
+        callback: function(position) {
+            // callback every position change
+            console.log(position);
+        }
+      });
