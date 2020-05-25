300a56c9afbc93cc9370ec1e88d875996ba576bc
emilbergs <emilberg@outlook.dk>
2020-05-25 09:57:47 +0200

    64

--- js/script.js
+++ js/script.js
@@ -85,3 +85,26 @@ submitb.addEventListener("click", changeInput);
 function changeInput(){
     inputtext.value = "Thank you!";
 }
+
+//api
+const getTextBtnElement = document.getElementById("getCurrentTimes");
+
+getTextBtnElement.addEventListener("click", getTimes);
+
+function getTimes(){
+    fetch('https://api.myjson.com/bins/1dgp04')
+    .then((res) => res.json())
+    .then((data) => {
+        let output = '';
+        data.forEach(function(bins){
+            output+= `
+
+<section class="tidspunk-flex">
+    <section class="tidspunkt-p"><p class="tidspunkt-p-klok">${bins.Time}</p></section>
+    </section>
+
+`;
+        })
+        document.getElementById("output").innerHTML = output;
+    })
+};
