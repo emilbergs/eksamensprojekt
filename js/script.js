d34e56e309bfcca4e344c6e59fdf8e071be0ef60
Neibye <60384297+Neibye@users.noreply.github.com>
2020-05-25 20:18:03 +0200

    Version 70
     API index

--- js/script.js
+++ js/script.js
@@ -94,7 +94,7 @@ function getFriday() {
     fetch('json/friday.json')
         .then((res) => res.json())
         .then((data) => {
-            let output= `
+            let output = `
 <h1 class="display mb-4">PROGRAM</h1>
 <h2 class="display mb-4">FRIDAY: 19:00 - 05:00</h2>`
             data.forEach(function (friday) {
@@ -104,8 +104,8 @@ function getFriday() {
 <li class="list-group-item">Name: ${friday.Name}</li>
 <li class="list-group-item">Act: ${friday.Act}</li>
 </ul>`
-            }) 
-        document.getElementById("output1").innerHTML = output;
+            })
+            document.getElementById("output1").innerHTML = output;
         })
 }
 
@@ -115,7 +115,7 @@ function getSaturday() {
     fetch('json/saturday.json')
         .then((res) => res.json())
         .then((data) => {
-            let output= `
+            let output = `
 <h2 class="display mb-4">SATURDAY: 14:00 - 05:00</h2>
 <h3 class="display mb-4" id="emilkankode">Dagsprogram (Free until 20:00)</h3>`
             data.forEach(function (saturday) {
@@ -125,8 +125,101 @@ function getSaturday() {
 <li class="list-group-item">Name: ${saturday.Name}</li>
 <li class="list-group-item">Act: ${saturday.Act}</li>
 </ul>`
-            }) 
-        document.getElementById("output2").innerHTML = output;
+            })
+            document.getElementById("output2").innerHTML = output;
         })
 }
 
+function getImages() {
+    fetch('json/images.json')
+        .then((res) => res.json())
+        .then((data) => {
+            let output = ``
+            data.forEach(function (images) {
+                output += `
+                    <div id="section2">
+                        <h2>HEADLINERS</h2>
+                        <div id="flex4">
+                            <img src="images/music1.png" class="tabletthings" id="banner1" alt="music">
+                            <div class="marginimages">
+                                <img src=${images.Music1IMG} alt="Jan Brandi">
+                                <div class="headlinerText">
+                                    <h3>${images.MusicName1}</h3>
+                                    <h4>${images.MusicDate1}</h4>
+                                </div>
+                            </div>
+                            <div class="tabletthings marginimages">
+                                <img src=${images.Music2IMG} alt="Casper Christensen">
+                                <div class="headlinerText">
+                                    <h3>${images.MusicName2}</h3>
+                                    <h4>${images.MusicDate2}</h4>
+                                </div>
+                            </div>
+                            <div class="pcthings marginimages">
+                                <img src=${images.Music3IMG} alt="Kool K Stolze">
+                                <div class="headlinerText">
+                                    <h3>${images.MusicName3}</h3>
+                                    <h4>${images.MusicDate3}</h4>
+                                </div>
+                            </div>
+                            <img src="images/music2.png" class="tabletthings" id="banner2" alt="music">
+                        </div>
+                    </div>
+                <section id="section3">
+                    <div id="flex5">
+                        <img src="images/art1.png" class="tabletthings" id="banner3" alt="art">
+                        <div class="marginimages">
+                            <img src=${images.Art1IMG} alt="Discosmos">
+                            <div class="headlinerText">
+                                <h3>${images.ArtName1}</h3>
+                                <h4>${images.ArtDate1}</h4>
+                            </div>
+                        </div>
+                        <div class="tabletthings marginimages">
+                            <img src=${images.Art2IMG} alt="Hackstage">
+                            <div class="headlinerText">
+                                <h3>${images.ArtName2}</h3>
+                                <h4>${images.ArtDate2}</h4>
+                            </div>
+                        </div>
+                        <div class="pcthings marginimages">
+                            <img src=${images.Art3IMG} alt="Panorama light art">
+                            <div class="headlinerText">
+                                <h3>${images.ArtName3}</h3>
+                                <h4>${images.ArtDate3}</h4>
+                            </div>
+                        </div>
+                        <img src="images/art2.png" class="tabletthings" id="banner4" alt="art">
+                    </div>
+                </section>
+                <section id="section4">
+                    <div id="flex6">
+                        <img src="images/shows1.png" class="tabletthings" id="banner5" alt="shows">
+                        <div class="marginimages">
+                            <img src=${images.Event1IMG} alt="Mobile Mirrors">
+                            <div class="headlinerText">
+                                <h3>${images.EventName1}</h3>
+                                <h4>${images.EventDate1}</h4>
+                            </div>
+                        </div>
+                        <div class="tabletthings marginimages">
+                            <img src=${images.Event2IMG} alt="Skeletonman">
+                            <div class="headlinerText">
+                                <h3>${images.EventName2}</h3>
+                                <h4>${images.EventDate2}</h4>
+                            </div>
+                        </div>
+                        <div class="pcthings marginimages">
+                            <img src=${images.Event3IMG} alt="Skeleton">
+                            <div class="headlinerText">
+                                <h3>${images.EventName3}</h3>
+                                <h4>${images.EventDate3}</h4>
+                            </div>
+                        </div>
+                        <img src="images/shows2.png" class="tabletthings" id="banner6" alt="shows">
+                    </div>
+                </section>`
+            })
+            document.getElementById("output3").innerHTML = output;
+        })
+}
