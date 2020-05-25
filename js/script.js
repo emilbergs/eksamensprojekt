698f8c0c6e124a7f754b47752f2ea48367c3bd36
emilbergs <emilberg@outlook.dk>
2020-05-25 11:01:33 +0200

    65
    need api

--- js/script.js
+++ js/script.js
@@ -86,25 +86,14 @@ function changeInput(){
     inputtext.value = "Thank you!";
 }
 
-//api
-const getTextBtnElement = document.getElementById("getCurrentTimes");
 
-getTextBtnElement.addEventListener("click", getTimes);
+//API
 
-function getTimes(){
-    fetch('https://api.myjson.com/bins/1dgp04')
-    .then((res) => res.json())
-    .then((data) => {
-        let output = '';
-        data.forEach(function(bins){
-            output+= `
 
-<section class="tidspunk-flex">
-    <section class="tidspunkt-p"><p class="tidspunkt-p-klok">${bins.Time}</p></section>
-    </section>
 
-`;
-        })
-        document.getElementById("output").innerHTML = output;
-    })
-};
+
+
+
+
+
+
